14585494cdce0f78a11d0fa2560d1125f8c7c41b
Edward Kmett <ekmett@gmail.com>
2018-10-01 23:14:25 -0400

    more work on unification and environments

--- src/Env.hs
+++ src/Env.hs
@@ -1,9 +1,11 @@
 {-# language DeriveTraversable #-}
 {-# language RankNTypes #-}
+{-# language LambdaCase #-}
 
-module Skew
+-- | Unsafe environment operations
+module Env
   ( Env(..)
-  , var
+  , var, var'
   , lookup
   , Nil(..)
   , Cons(..)
@@ -11,10 +13,12 @@ module Skew
   , allocate
   ) where
 
+import Control.Lens (fusing, Lens')
 import Data.Bits
+import Data.Functor
 import Data.Semigroup
-import Unaligned
 import Prelude hiding (lookup)
+import Unaligned
 
 data Tree a
   = Bin a !(Tree a) !(Tree a)
@@ -29,17 +33,33 @@ bin :: Maybe a -> Tree a -> Tree a -> Tree a
 bin Nothing Tip Tip = Tip
 bin Nothing l r = Bin_ l r
 bin (Just a) l r = Bin a l r
+{-# inline conlike bin #-}
 
 bin_ :: Tree a -> Tree a -> Tree a
 bin_ Tip Tip = Tip
 bin_ l r = Bin_ l r
+{-# inline conlike bin_ #-}
+
+padSpine :: Int -> Spine a -> Spine a
+padSpine 0 xs = xs
+padSpine i xs@(Cons j x ys@(Cons k y zs))
+  -- climb up and inflate heads as needed
+  | j == k   = padSpine (i-1)   $ Cons (j+k+1) (bin_ x y) zs
+  | i >= j+1 = padSpine (i-j+1) $ Cons (j+j+1) (bin_ Tip x) ys
+  | otherwise = go i (unsafeShiftR j 1) xs
+  where
+    go i j ws
+      | i >= j = go (i - j) j $ Cons j Tip ws
+      | otherwise = go i (unsafeShiftR j 1) ws
 
 instance Nil Spine where
   nil = Nil
+  {-# inline conlike nil #-}
 
 instance Cons Spine where
   cons a (Cons i x (Cons j y zs)) | i == j = Cons (i+j+1) (Bin a x y) zs
   cons a xs = Cons 1 (Bin a Tip Tip) xs
+  {-# inline conlike cons #-}
 
 class Cons_ t where
   cons_ :: t a -> t a
@@ -47,21 +67,25 @@ class Cons_ t where
 instance Cons_ Spine where
   cons_ (Cons i x (Cons j y zs)) | i == j = Cons (i+j+1) (bin_ x y) zs
   cons_ xs = Cons 1 Tip xs
+  {-# inline conlike cons_ #-}
 
 data Env a = Env {-# unpack #-} !Int {-# unpack #-} !Int !(Spine a)
   deriving Show
 
 instance Nil Env where
   nil = Env 0 0 nil
+  {-# inline conlike nil #-}
 
 instance Cons Env where
-  cons a (Env i j xs) = Env (i+1) (j+1) (cons a xs)
+  cons a (Env i j xs) = Env (i+1) (i+1) (cons a (padSpine (i-j) xs))
+  {-# inline conlike cons #-}
 
 instance Cons_ Env where
-  cons_ (Env i j xs) = Env (i+1) (j+1) (cons_ xs)
+  cons_ (Env i j xs) = Env (i+1) j xs
+  {-# inline conlike cons_ #-}
 
 allocate :: Int -> Env a -> Env a
-allocate i (Env j k xs) = Env (j+i) (k+i) $ appEndo (stimesMonoid i (Endo cons_)) xs
+allocate i (Env j k xs) = Env (j+i) k xs
 
 lookup :: Int -> Env a -> Maybe a
 lookup i (Env j k xs)
@@ -88,16 +112,18 @@ lookupTree i j (Bin_ l r)
   | otherwise = lookupTree (i-j'-1) j' r
   where j' = unsafeShiftR j 1
 
-type Lens' s a = forall f. Functor f => (a -> f a) -> s -> f s
-
--- TODO: actualVar i = fusing (var i)
+-- type Lens' s a = forall f. Functor f => (a -> f a) -> s -> f s
 
 var :: Int -> Lens' (Env a) (Maybe a)
-var i f (Env j k xs)
+var = fusing . var'
+
+var' :: Int -> Lens' (Env a) (Maybe a)
+var' i f (Env j k xs)
   | i > j     = error "variable impossibly new"
-  -- | i > k     = <$> f Nothing now we need to extend to the left
+  | i > k     = f Nothing <&> \case
+    Nothing -> Env j k xs
+    Just a  -> Env j (i+1) $ cons a $ padSpine (i - k - 1) xs
   | otherwise = Env j k <$> varSpine (k - i - 1) f xs
-{-# inline var #-}
 
 varSpine :: Int -> Lens' (Spine a) (Maybe a)
 varSpine _ _ Nil = error "variable impossibly old"
@@ -105,6 +131,7 @@ varSpine i f (Cons j t xs)
   | i < j     = (\t' -> Cons j t' xs) <$> varTree i j f t
   | otherwise = Cons j t <$> varSpine (i - j) f xs
 
+-- place a fresh leaf down inside a tree appropriately
 tweak :: Int -> Int -> Maybe a -> Tree a
 tweak _ _ Nothing = Tip
 tweak i0 j0 (Just a0) = go i0 j0 a0 where
--- src/Freer.hs
+++ src/Freer.hs
@@ -1,11 +1,13 @@
 {-# language GADTs #-}
 {-# language DeriveTraversable #-}
+{-# language ExistentialQuantification #-}
 
-module Free where
+module Freer where
 
 import Control.Arrow (Kleisli(..))
 import Control.Monad (ap, liftM)
 import Control.Category
+import Data.Functor 
 import Prelude hiding ((.),id)
 
 import Aligned
@@ -13,21 +15,35 @@ import Aligned
 data Free f a where
   F :: FreeView f x -> Rev Cat (Kleisli (Free f)) x b -> Free f b
 
-data FreeView f a = Pure a | Free (f (Free f a))
-  deriving (Functor, Foldable, Traversable)
+data FreeView f a
+  = Pure a
+  | forall x. Free (f x) (x -> Free f a)
 
-view :: Functor f => Free f a -> FreeView f a
+instance Functor (FreeView f) where
+  fmap f (Pure a) = Pure (f a)
+  fmap f (Free fx k) = Free fx (fmap f . k)
+
+instance Foldable f => Foldable (FreeView f) where
+  foldMap f (Pure a) = f a
+  foldMap f (Free fx k) = foldMap (foldMap f . k) fx
+
+instance Traversable f => Traversable (FreeView f) where
+  traverse f (Pure a) = Pure <$> f a
+  traverse f (Free fx k)
+    = traverse (traverse f . k) fx <&> \fx' -> Free fx' id
+
+view :: Free f a -> FreeView f a
 view (F h t) = case h of
   Pure a -> case unsnoc t of
     Empty     -> Pure a
     tc :&: hc -> view $ runKleisli hc a ^>>= tc
-  Free f -> Free $ fmap (^>>= t) f
+  Free fx k -> Free fx ((^>>= t) . k)
 
 unview :: FreeView f a -> Free f a
 unview x = F x id
 
 free :: f (Free f a) -> Free f a
-free fx = F (Free fx) id
+free fx = F (Free fx id) id
 
 (^>>=) :: Free f x -> Rev Cat (Kleisli (Free f)) x b -> Free f b
 F h t ^>>= r = F h (r . t)
@@ -35,7 +51,7 @@ F h t ^>>= r = F h (r . t)
 instance Functor (Free f) where
   fmap = liftM
 
-instance (Functor f, Foldable f) => Foldable (Free f) where
+instance Foldable f => Foldable (Free f) where
   foldMap f = foldMap f . view
 
 instance Traversable f => Traversable (Free f) where
