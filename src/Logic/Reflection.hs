ded6b7ef3fe30114afb993b21b2f41ca43714512
Edward Kmett <ekmett@gmail.com>
2018-10-01 20:43:01 -0400

    L m a is redundant, use View

--- src/Logic/Reflection.hs
+++ src/Logic/Reflection.hs
@@ -7,30 +7,40 @@ module Logic.Reflection where
 import Control.Monad
 import Control.Monad.Trans
 import Control.Applicative
+import Data.Bifunctor
+import Data.Bifoldable
+import Data.Bitraversable
 import Data.Functor.Identity
 import Logic.Class
 import Unaligned
 
-data L m a = Nil | Cons a (LogicT m a)
-  deriving (Functor, Foldable, Traversable)
+type L m a = View a (LogicT m a)
 
 newtype LogicT m a = LogicT { runLogicT :: Cat (m (L m a)) }
-  deriving (Functor, Foldable, Traversable)
+
+instance Functor m => Functor (LogicT m) where
+  fmap f = LogicT . fmap (fmap (bimap f (fmap f))) . runLogicT
+
+instance Foldable m => Foldable (LogicT m) where
+  foldMap f = foldMap (foldMap (bifoldMap f (foldMap f))) . runLogicT
+
+instance Traversable m => Traversable (LogicT m) where
+  traverse f = fmap LogicT . traverse (traverse (bitraverse f (traverse f))) . runLogicT
 
 type Logic = LogicT Identity
 
 single :: Monad m => a -> m (L m a)
-single a = return (Cons a empty)
+single a = return (a :&: empty)
 
 unview :: m (L m a) -> LogicT m a
 unview = LogicT . singleton
 
 view :: Monad m => LogicT m a -> m (L m a)
 view (LogicT s) = case uncons s of
-   Empty -> return Nil
+   Empty -> return Empty
    h :&: t -> h >>= \case
-     Nil -> view (LogicT t)
-     Cons hi (LogicT ti) -> return $ Cons hi $ LogicT $ ti <> t
+     Empty -> view (LogicT t)
+     hi :&: LogicT ti -> return $ hi :&: LogicT (ti <> t)
 
 instance Monad m => Applicative (LogicT m) where
   pure = unview . single
@@ -38,14 +48,13 @@ instance Monad m => Applicative (LogicT m) where
 
 instance Monad m => Alternative (LogicT m) where
   empty = LogicT mempty
-  (view -> m) <|> n = unview $ m >>= return . \case
-    Nil -> Nil
-    Cons h t -> Cons h (LogicT $ runLogicT t <> runLogicT n) 
+  m <|> LogicT n = unview $
+    fmap (\(LogicT t) -> LogicT $ t <> n) <$> view m
 
 instance Monad m => Monad (LogicT m) where
-  (view -> m) >>= f = unview $ m >>= \case
-    Nil      -> return Nil
-    Cons h t -> view $ f h <|> (t >>= f)
+  m >>= f = unview $ view m >>= \case
+    Empty -> return Empty
+    h :&: t -> view $ f h <|> (t >>= f)
   fail _ = mzero
 
 instance Monad m => MonadPlus (LogicT m) where
@@ -56,21 +65,17 @@ instance MonadTrans LogicT where
   lift m = unview (m >>= single)
 
 instance MonadIO m => MonadIO (LogicT m) where
-    liftIO = lift . liftIO
-
-simple :: L m a -> View a (LogicT m a)
-simple Nil = Empty
-simple (Cons a as) = a :&: as
+  liftIO = lift . liftIO
 
 instance Monad m => MonadLogic (LogicT m) where
-  msplit (view -> m) = lift (simple <$> m)
+  msplit = lift . view
 
 observeAllT :: Monad m => LogicT m a -> m [a]
-observeAllT (view -> m) = m >>= get where
-  get (Cons a t) = liftM (a :) (observeAllT t)
-  get _          = return []
+observeAllT m = view m >>= go where
+  go (a :&: t) = (a:) <$> observeAllT t
+  go _ = return []
 
 observeT :: Monad m => LogicT m a -> m a
-observeT (view -> m) = m >>= get where
-  get (Cons a _) = return a
-  get _          = fail "No results"
+observeT m = view m >>= go where
+  go (a :&: _) = return a
+  go _ = fail "No results"
