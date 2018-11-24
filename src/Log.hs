2394411289a390aefde8b3d627b1af0d572dabf5
Edward Kmett <ekmett@gmail.com>
2018-11-24 02:06:08 -0500

    "mutable" Log and Cursor

--- src/Log.hs
+++ src/Log.hs
@@ -1,12 +1,29 @@
+{-# language FlexibleContexts #-}
 {-# language FlexibleInstances #-}
 {-# language FunctionalDependencies #-}
 {-# language BangPatterns #-}
 {-# language TemplateHaskell #-}
+{-# language MultiWayIf #-}
+{-# language RankNTypes #-}
+{-# language GADTs #-}
 
-module Log where
+module Log 
+  ( 
+  -- * Logs
+    Log
+  , newLog, cursors, record
+  -- * Cursors
+  , Cursor, newCursor, oldCursor, deleteCursor, advance
+  ) where
 
+import Control.Monad (join)
+import Control.Monad.State.Class
 import Control.Lens
 import Data.FingerTree as F
+import Data.Foldable as Foldable
+import Key
+import Ref
+import Prelude hiding (log)
 
 -- version # since, ref count, monoidal summary
 data LogEntry a = LogEntry
@@ -29,36 +46,106 @@ instance Monoid a => Monoid (LogEntry a) where
 -- cell
 --
 -- TODO: allow some form of properly filtered threshold reads in the log
-data Log a = Log
+data LogState a = LogState
   { logTree               :: !(FingerTree (LogEntry a) (LogEntry a))
   , logSince, logRefCount :: {-# UNPACK #-} !Int
   , logChanges            :: !(Maybe a)
   } deriving Show
 
-makeClassy ''Log
-
 -- | Argument tracks if we're persistent or not. If persistent then 'oldCursors' can start at the beginning
 -- otherwise we won't collect history beyond what is needed to support active cursors.
-newLog :: Monoid a => Bool -> Log a
-newLog p = Log mempty 0 (fromEnum p) Nothing
+newLogState :: Monoid a => Bool -> LogState a
+newLogState p = LogState mempty 0 (fromEnum p) Nothing
 
-record :: Semigroup a => a -> Log a -> Log a
-record a ls@(Log t v c m)
+recordState :: Semigroup a => a -> LogState a -> LogState a
+recordState a ls@(LogState t v c m)
   | F.null t, c == 0 = ls -- nobody is watching
-  | otherwise = Log t v c $ Just $ maybe a (<> a) m
+  | otherwise = LogState t v c $ Just $ maybe a (<> a) m
+
+--cursors :: Monoid a => LogState a -> Int
+--cursors (LogState t _ c _) = refCount (measure t) + c
+
+watchNew :: Monoid a => LogState a -> (Int, LogState a)
+watchNew (LogState t v c m) = case m of
+  Nothing -> (v, LogState t v (c+1) Nothing)
+  Just a  -> (v', LogState (t F.|> LogEntry v c a) v' 1 Nothing) where !v' = v + 1
+
+-- clone the oldest version in the log, or the beginning of time if newLogState True was used to construct this
+watchOld :: Monoid a => LogState a -> (Int, LogState a)
+watchOld (LogState t v c m) = case viewl t of
+  EmptyL -> (v, LogState t v (c+1) m)
+  LogEntry ov oc a F.:< t' -> (ov, LogState (LogEntry ov (oc+1) a F.<| t') v c m)
+ 
+data Log u a where
+  Log :: Monoid a => { getLog :: Ref u (LogState a) } -> Log u a
+
+log :: HasRefEnv s u => Log u a -> Lens' s (LogState a)
+log = ref . getLog
+
+newLog
+  :: (MonadState s m, MonadKey m, HasRefEnv s (KeyState m), Monoid a)
+  => Bool
+  -> m (Log (KeyState m) a)
+newLog = fmap Log . newRef . newLogState
 
 -- | Get a snapshot of the # of cursors outstanding
-cursors :: Monoid a => Log a -> Int
-cursors (Log t _ c _) = refCount (measure t) + c
+cursors :: (MonadState s m, HasRefEnv s u, Monoid a) => Log u a -> m Int
+cursors l = uses (log l) $ \(LogState t _ c _) -> refCount (measure t) + c 
 
-watchNew :: Monoid a => Log a -> (Int, Log a)
-watchNew (Log t v c m) = case m of
-  Nothing -> (v, Log t v (c+1) Nothing)
-  Just a  -> (v', Log (t F.|> LogEntry v c a) v' 1 Nothing) where !v' = v + 1
+record :: (MonadState s m, HasRefEnv s u, Semigroup a) => Log u a -> a -> m ()
+record (Log r) a = modifyRef r (recordState a)
 
--- clone the oldest version in the log, or the beginning of time if newLog True was used to construct this
-watchOld :: Monoid a => Log a -> (Int, Log a)
-watchOld (Log t v c m) = case viewl t of
-  EmptyL -> (v, Log t v (c+1) m)
-  LogEntry ov oc a F.:< t' -> (ov, Log (LogEntry ov (oc+1) a F.<| t') v c m)
+data Cursor u a = Cursor {-# unpack #-} !(Log u a) {-# unpack #-} !(Ref u Int)
 
+-- data Cursor a = Cursor !(Var a) {-# UNPACK #-} !Int
+
+-- | Subscribe to _new_ updates, but we won't get the history.
+newCursor, oldCursor :: (MonadState s m, HasRefEnv s (KeyState m), MonadKey m, Monoid a) => Log (KeyState m) a -> m (Cursor (KeyState m) a)
+newCursor l = Cursor l <$> do
+  i <- log l %%= watchNew 
+  newRef i
+
+oldCursor l = Cursor l <$> do
+  i <- log l %%= watchOld
+  newRef i
+
+-- invalidates this cursor
+deleteCursor :: (MonadState s m, HasRefEnv s u) => Cursor u a -> m ()
+deleteCursor (Cursor rl@Log{} ri) = do
+  i <- readRef ri
+  unsafeDeleteRef ri -- we're done, invalidate this cursor
+  log rl %= \ls@(LogState t v c m) -> if
+    | i >= v -> LogState t v (c-1) m
+    | otherwise -> case F.split (\(LogEntry j _ _) -> j >= i) t of
+      (l,r) -> case viewr l of
+        EmptyR -> ls
+        l' F.:> LogEntry j c' a -> LogState (nl >< r) v c m where
+          nl | c' > 1 = l' F.|> LogEntry j (c'-1) a
+             | otherwise = case viewr l' of
+               EmptyR -> mempty
+               l'' F.:> LogEntry k c'' b -> l'' F.|> LogEntry k c'' (mappend b a)
+
+-- (0{2} m) (2{3} m) (3{4}) m {1} Nothing
+
+-- 0 m 1 m {2 mempty} 3 m 4 m {5 mempty} {6 mempty} {7 mempty} 8 m {9 mempty} 10 Maybe
+-- {}'d things are implied
+advance :: (MonadState s m, HasRefEnv s u) => Cursor u a -> m a
+advance (Cursor rl@Log{} ri) = do
+  i <- readRef ri 
+  join $ log rl %%= \ls@(LogState t v c m) -> if
+    | i >= v -> case m of
+      Nothing -> (pure mempty, ls)
+      Just a
+        | c == 1 -> case viewr t of
+          t' F.:> LogEntry ov oc b -> (pure a , LogState (t' F.|> LogEntry ov oc (mappend b a)) v 1 Nothing)
+          EmptyR -> (pure a, LogState t v c Nothing) -- we're the only one listening
+        | !v' <- v + 1 -> (a <$ writeRef ri v', LogState (t F.|> LogEntry v (c-1) a) v' 1 Nothing)
+    | otherwise -> case F.split (\(LogEntry j _ _) -> j >= i) t of
+      (l,r) -> case viewr l of
+        EmptyR -> error "advance: missing cursor!"
+        l' F.:> LogEntry j c' a
+          | (ls', k) <- watchNew (LogState (nl >< r) v c m) -> ((a <> contents (measure r) <> Foldable.fold m) <$ writeRef ri ls', k) where
+          nl | c' > 1 = l' F.|> LogEntry j (c'-1) a
+             | otherwise = case viewr l' of
+               l'' F.:> LogEntry k c'' b -> l'' F.|> LogEntry k c'' (mappend b a)
+               EmptyR -> mempty
