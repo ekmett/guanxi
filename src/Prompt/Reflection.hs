3217d64412f5fc0a282b44d095710e92889ef2a8
Edward Kmett <ekmett@gmail.com>
2018-12-04 21:50:54 -0500

    more reflection refactoring

--- src/Prompt/Reflection.hs
+++ src/Prompt/Reflection.hs
@@ -3,6 +3,7 @@
 {-# language GADTs #-}
 {-# language MultiParamTypeClasses #-}
 {-# language RankNTypes #-}
+{-# language ScopedTypeVariables #-}
 {-# language TemplateHaskell #-}
 {-# language TypeFamilies #-}
 {-# language UndecidableInstances #-}
@@ -10,44 +11,42 @@
 module Prompt.Reflection
   ( CC
   , runCC
-  , runCC_ -- falls back on MonadFail on missing prompt
+  , runCC_
   ) where
 
 import Aligned.Base
 import Control.Arrow (Kleisli(..))
 import Control.Category
-import Control.Monad (ap)
+import Control.Monad as Monad
 import Control.Monad.Fail as MonadFail
 import Control.Monad.IO.Class
 import Control.Monad.Primitive
 import Control.Monad.State.Class
 import Control.Monad.Trans.Class
-import Data.Default
-import Data.Functor ((<&>))
 import Data.Type.Equality
 import Prelude hiding (id,(.))
 import Prompt.Class
 import Ref.Key
+import qualified Unaligned.Base as U
 
-type K m = Rev Cat (Kleisli m)
+type P m = Key (KeyState m) -- prompts
+type KC m = Rev Cat (Kleisli (CC m))
+type DC m = Rev Cat (Del m)
 
-data Del m a b = Del {-# unpack #-} !(Key (KeyState m) b) !(K (CC m) a b)
-
-type Dels m = Rev Cat (Del m)
+data Del m a b = Del {-# unpack #-} !(P m b) !(KC m a b)
 
 data SC m a b where
-  SC :: !(Dels m a w) -> !(K (CC m) w b) -> SC m a b
+  SC :: !(DC m a w) -> !(KC m w b) -> SC m a b
 
 data CC m a where
-  WithSC
-    :: {-# unpack #-} !(Key (KeyState m) x)
-    -> (SC m w x -> CC m x)
-    -> {-# unpack #-} !(SC m w a) -> CC m a
-  CC :: m w -> {-# unpack #-} !(SC m w a) -> CC m a
+  WithSC :: {-# unpack #-} !(P m x) -> (SC m w x -> CC m x) -> !(DC m w y) -> !(KC m y a) -> CC m a
+  CC :: m w -> !(DC m w y) -> !(KC m y a) -> CC m a
 
 mapSC :: (forall w. SC m w a -> SC m w b) -> CC m a -> CC m b
-mapSC f (WithSC cp ck s) = WithSC cp ck (f s)
-mapSC f (CC p s) = CC p (f s)
+mapSC f (WithSC cp ck sd sk) = case f (SC sd sk) of
+  SC sd' sk' -> WithSC cp ck sd' sk'
+mapSC f (CC p sd sk) = case f (SC sd sk) of
+  SC sd' sk' -> CC p sd' sk'
 {-# inline mapSC #-}
 
 instance Category (SC m) where
@@ -55,68 +54,97 @@ instance Category (SC m) where
   SC cat_bw kwc . SC cat_aw1 kw1b = case unsnoc cat_bw of
     Empty -> SC cat_aw1 (kwc . kw1b)
     t :&: Del p h -> SC (snoc t (Del p (h . kw1b)) . cat_aw1) kwc
+  {-# inline (.) #-}
 
 (!>>=) :: CC m a -> SC m a b -> CC m b
 m !>>= f = mapSC (f.) m
+{-# inline (!>>=) #-}
 
 instance Monad m => Functor (CC m) where
-  fmap f m = m !>>= SC def (singleton (Kleisli (pure . f)))
+  fmap = liftM
+  {-# inlineable fmap #-}
 
 instance Monad m => Applicative (CC m) where
-  pure a = CC (pure a) id
+  pure a = CC (pure a) id id
+  {-# inlineable pure #-}
   (<*>) = ap
+  {-# inlineable (<*>) #-}
 
 instance Monad m => Monad (CC m) where
-  m >>= f = m !>>= SC def (singleton (Kleisli f))
+  WithSC cp ck sd sk >>= f = WithSC cp ck sd (cons (Kleisli f) sk)
+  CC p sd sk >>= f = CC p sd (cons (Kleisli f) sk)
+  {-# inlineable (>>=) #-}
+
+  fail = lift . Monad.fail
+ 
+instance MonadFail m => MonadFail (CC m) where
+  fail = lift . MonadFail.fail
 
 instance MonadIO m => MonadIO (CC m) where
   liftIO = lift . liftIO
+  {-# inlineable liftIO #-}
 
 instance PrimMonad m => PrimMonad (CC m) where
   type PrimState (CC m) = PrimState m
   primitive f = lift (primitive f)
+  {-# inlineable primitive #-}
 
 instance MonadKey m => MonadKey (CC m) where
   type KeyState (CC m) = KeyState m
 
 instance MonadTrans CC where
-  lift m = CC m id
+  lift m = CC m id id
+  {-# inlineable lift #-}
 
 instance MonadKey m => MonadPrompt (CC m) where
-  type Prompt (CC m) = Key (KeyState m)
+  type Prompt (CC m) = P m
   type Sub (CC m) = SC m
-  newPrompt = CC newKey id
+  newPrompt = CC newKey id id
+  {-# inlineable newPrompt #-}
   pushPrompt p = mapSC $ \(SC d t) -> SC (cons (Del p t) d) id
-  withSub p f = WithSC p f id
+  {-# inlineable pushPrompt #-}
+  withSub p f = WithSC p f id id
+  {-# inlineable withSub #-}
   pushSub = flip (!>>=)
+  {-# inlineable pushSub #-}
 
 instance MonadState s m => MonadState s (CC m) where
   get = lift get
+  {-# inline get #-}
   put = lift . put
+  {-# inline put #-}
   state = lift . state
+  {-# inline state #-}
 
-runCC :: Monad m => m a -> CC m a -> m a
-runCC err (CC m r) = m >>= runSC err r
-runCC err (WithSC p f (SC d l)) = case split p d of
-  Just (sc, r) -> runCC err $ f sc !>>= SC r l
-  Nothing -> err
+runCC :: forall m a. Monad m => m a -> CC m a -> m a
+runCC e = goCC where
+  goCC :: CC m a -> m a
+  goCC (CC m d l) = m >>= goSC d l
+  goCC (WithSC p f d l) = case split p d of
+    sc U.:&: r -> goCC $ f sc !>>= SC r l
+    U.Empty -> e
+
+  goSC :: Monad m => DC m w x -> KC m x a -> w -> m a
+  goSC d l x = case unsnoc d of
+    Empty -> case unsnoc l of
+      Empty -> pure x
+      t :&: Kleisli f -> goCC $! case f x of
+        WithSC cp ck sd sk -> WithSC cp ck sd (t . sk)
+        CC p sd sk -> CC p sd (t . sk)
+    t :&: Del p h -> case unsnoc h of
+      Empty -> goSC t l x
+      ti :&: Kleisli f -> goCC $ f x !>>= SC (t `snoc` Del p ti) l
+{-# inlineable runCC #-}
+
+split :: Key (KeyState m) w -> DC m a b -> U.View (SC m a w) (DC m w b)
+split p q = case unsnoc q of
+  Empty -> U.Empty
+  t :&: Del p' sk -> case testEquality p p' of
+    Just Refl -> SC id sk U.:&: t
+    Nothing -> case split p t of
+      SC dl tl U.:&: sk' -> SC (dl `snoc` Del p' sk) tl U.:&: sk'
+      U.Empty -> U.Empty
 
 runCC_ :: MonadFail m => CC m a -> m a
 runCC_ = runCC $ MonadFail.fail "missing prompt"
-
-runSC :: Monad m => m a -> SC m w a -> w -> m a
-runSC err (SC d l) x = case unsnoc d of
-  Empty -> case unsnoc l of
-    Empty -> pure x
-    t :&: Kleisli f -> runCC err $ f x !>>= SC id t
-  t :&: Del p h -> case unsnoc h of
-    Empty -> runSC err (SC t l) x
-    ti :&: Kleisli f -> runCC err $ f x !>>= SC (t `snoc` Del p ti) l
-
-split :: Key (KeyState m) w -> Dels m a b -> Maybe (SC m a w, Dels m w b)
-split p q = case unsnoc q of
-  Empty -> Nothing
-  t :&: Del p' sk -> case testEquality p p' of
-    Just Refl -> Just (SC id sk, t)
-    Nothing -> split p t <&> \ (SC dl tl, sk') ->
-      (SC (dl `snoc` Del p' sk) tl, sk')
+{-# inlineable runCC_ #-}
