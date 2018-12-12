09a79d7f66d905a5b06c0402f8918ebb0a9f6b5c
Edward Kmett <ekmett@gmail.com>
2018-12-12 18:30:01 -0500

    rename torus -> dlx, proper dynamic items

--- cbits/dlx.cpp
+++ cbits/dlx.cpp
@@ -9,53 +9,56 @@
 
 using namespace std;
 
+typedef uint32_t option;
+
 struct cell {
-  uint32_t parity:1, item:31,u,d;
+  uint32_t parity:1, item:31, u, d;
   cell(int32_t parity=0, uint32_t item=0, uint32_t u=0, uint32_t d=0)
   : parity(parity), item(item), u(u), d(d) {}
 };
 
 struct item {
-  uint32_t p,n,cell,count;
+  uint32_t p, n, cell, count;
   item(uint32_t p=0, uint32_t n=0, uint32_t cell=0, uint32_t count=0)
   : p(p), n(n), cell(cell), count(count) {}
 };
 
-struct torus {
+
+struct dlx {
   std::vector<cell> cells;
   std::vector<item> items;
-  std::vector<uint32_t> result; // rows
+  std::vector<option> result; // rows
 
-  torus(uint32_t n=0, uint32_t k=0) noexcept
+  dlx(uint32_t n=0, uint32_t k=0) noexcept
   : cells(0)
   , items(0)
   , result(0) {
-    uint32_t N = n + k;
-    N = N ? N : 1;
-    cells.reserve(N+1);
+    uint32_t N = n+k;
+    cells.reserve(N?N+1:2);
     items.reserve(N+1);
 
-    for (uint32_t i=0;i<N;++i)
+    for (uint32_t i=0;i<n;++i) {
       cells.emplace_back(1,i,i,i);
-
-    cells.emplace_back(0); // sentinel
-
-    // link items
-    for (uint32_t i=0;i<n;++i)
       items.emplace_back((i+n-1)%n, (i+1)%n, i, 0);
+    }
 
-    // link optional items to themselves
-    for (uint32_t i=n;i<N;++i)
+    for (uint32_t i=n;i<N;++i) {
+      cells.emplace_back(1,i,i,i);
       items.emplace_back(i,i,i,0);
+    }
+
+    cells.emplace_back(0); // terminating sentinel
 
     if (n) {
-      items.emplace_back(n-1,0,N,0);
+      items.emplace_back(n-1,0);
       items[0].p = items[n-1].n = N;
     } else {
-      items.emplace_back(N,N,N,0);
+      items.emplace_back(N,N);
     }
   }
 
+  // dynamically add k items to the problem that must be satisfied
+	// but can only be satisfied once
   uint32_t add_items(uint32_t k) {
     if (!k) return items.size()-1; // how many columns do we have
 
@@ -75,31 +78,22 @@ struct torus {
 
     uint32_t i = 0;
     for (;i<k;++i) {
-			// new header
       cells.emplace_back(parity,itembase+i,cellbase+i,cellbase+i);
-      items.emplace_back(
-			  i ? itembase+(i+k-1)%k : itembase+k,
-				itembase+(i+1)%k,
-				cellbase+i,
-				0
-			 );
+      items.emplace_back(i ? itembase+(i+k-1)%k : itembase+k, itembase+(i+1)%k, cellbase+i, 0);
 		}
-    items.emplace_back(
-		  was_empty ? itembase+k-1 : items[n].p,
-			itembase,
-			0,
-			0
-	  );
+    items.emplace_back(was_empty ? itembase+k-1 : items[n].p, itembase, 0, 0);
     if (!was_empty) {
       items[n].p = itembase+k-1;
       items[itembase+k-1].n = n;
 		} else {
-			items[itembase+k-1].n = itembase+k; // root
+			items[itembase+k-1].n = itembase+k;
 		}
     cells.emplace_back(~parity);
     return itembase;
 	}
 
+  // dynamically add k items to the problem that may be satisfied
+	// but can only be satisfied once
   uint32_t add_optional_items(uint32_t k) {
     if (!k) return items.size()-1; // how many columns do we have
     auto cellbase = cells.size()-1;
@@ -123,13 +117,15 @@ struct torus {
     return itembase;
 	}
 
-  template <typename T>
-  uint32_t add_option(T values) {
+  // add an option to the
+  template <typename Iterator>
+  uint32_t add_option(Iterator first, Iterator last) {
     auto base = cells.size()-1;
     auto parity = cells[base].parity;
     cells.pop_back(); // drop terminating sentinel
     uint32_t i = 0;
-    for (auto const & j : values) {
+    for (Iterator it = first; it != last; ++it) {
+      uint32_t j = *it;
       assert(j < items.size()-1); // exclude root
       auto u = cells[j].u;
       cells.emplace_back(parity,j,u,j);
@@ -140,20 +136,13 @@ struct torus {
     return base;
   }
 
+  template <typename T>
+  uint32_t add_option(T values) {
+    return add_option(values.begin(),values.end());
+  }
+
   uint32_t add_option(std::initializer_list<uint32_t> values) noexcept {
-    auto base = cells.size()-1;
-    auto parity = cells[base].parity;
-    cells.pop_back(); // drop terminating sentinel
-    uint32_t i = 0;
-    for (auto const & j : values) {
-      assert(j < items.size()-1); // exclude root
-      auto u = cells[j].u;
-      cells.emplace_back(parity,j,u,j);
-      cells[u].d = cells[j].u = base + i++;
-      ++items[cells[j].item].count; // bump counts of the columns
-    }
-    cells.emplace_back(!parity);
-    return base;
+    return add_option(values.begin(),values.end());
   }
   
   template <typename Fn> 
@@ -269,13 +258,13 @@ struct torus {
 };
 
 void queens(uint32_t n) {
-  auto organ = [&](int i) { return (i&1?n-1-i:n+i)>>1; };
-  uint32_t nn = n+n-2;
-  auto x = torus();
+  auto x = dlx();
 	auto rows = x.add_items(n);
   auto cols = x.add_items(n);
+  uint32_t nn = n+n-2;
 	auto a = x.add_optional_items(nn);
 	auto b = x.add_optional_items(nn);
+  auto organ = [&](int i) { return (i&1?n-1-i:n+i)>>1; };
   std::vector<uint32_t> option;
   for(uint8_t j=0;j<n;++j) {
     option.resize(0);
@@ -289,7 +278,7 @@ void queens(uint32_t n) {
       if (t && t < nn) option.emplace_back(a+t);
       t = n-1-r+c;
       if (t && t < nn) option.emplace_back(b+t);
-      x.add_option(option);
+      x.add_option(option.begin(),option.end());
     }
   }
   x.solve([&](const std::vector<uint32_t> & is) {
